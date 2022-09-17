ee13816cfc9faec457f8fb7112ae0d8b000b432a
Rajiv Taro <rajivtarox@gmail.com>
2022-09-17 21:10:06 +0530

    added infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -3,6 +3,7 @@ import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import { PropTypes } from 'prop-types';
 import './style.css';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 
@@ -22,16 +23,17 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
+
     }
 
     //async function returns a promise and await can be used within the function. adding await as a prefix of a function will cause the async function to pause until the await function is resolved.
 
     async getDetails() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c0750cc0b899482e82096cfc35f941e0&page=${this.state.page}&pagesize=${this.props.pageSize}`;
-        this.setState({ loading: true });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=49e0395ed3eb4228b6380f6ea3ffb9b4&page=${this.state.page}&pagesize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
@@ -42,34 +44,42 @@ export class News extends Component {
         this.getDetails();
     }
 
-    handlePrevClick = () => {
-        this.setState({ page: this.state.page - 1 }, this.getDetails); //here the second parameter is an optional callback function of setState which will only be executed after the state of 'page' is updated.
-        //alernatively, async/await can be used by adding await before this.setState and calling the function after that.
-    }
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=49e0395ed3eb4228b6380f6ea3ffb9b4&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults });
 
-    handleNextClick = () => {
-        this.setState({ page: this.state.page + 1 }, this.getDetails);
-    }
+    };
 
     render() {
         return (
             <div className='container my-3'>
                 <h1 className='text-center my-5' style={{ fontFamily: 'Baloo Bhai 2', cursor: 'default', textTransform: 'uppercase' }}> {this.props.category}</h1>
-                {this.state.loading && <Spinner />} 
-                {/* spinner will be rendered while the state of loading is true. loading will be true while the data is being fetched using api and that data is getting parsed (in getDetails function) */}
-                <div className="row">
-                    {/* map the elements of articles after loading becomes false (after fetching and parsing data in getDetails function) */}
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div key={element.url} className="col-md-4">
-                            <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source} />
-                        </div>
-                    })}
+                {/* loading state is initially true and then changed to false inside componentDidMount function (that calls getDetails function) */}
+                {this.state.loading && <Spinner />}
 
-                </div>
-                <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
+                {/* infinite scroll component copied from npmjs.com */}
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length < this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    {/* row div has been put inside container div so that the scroll bar won't appear. container div centers the elements inside. */}
+                    <div className="container">
+                        <div className="row">
+                            {/* map the elements of articles after loading becomes false (after fetching and parsing data in getDetails function) */}
+                            {this.state.articles.map((element) => {
+                                return <div key={element.url} className="col-md-4">
+                                    <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source} />
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
             </div>
         )
     }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import loading from './loading.gif'
 export default class Spinner extends Component {
   render() {
     return (
-      <div className='text-center' style={{height: '10rem', margin: '5rem auto'}}>
+      <div className='text-center' style={{margin: '5rem auto'}}>
         <img src={loading} alt='loading' style={{height: '100%'}}/>
       </div>
     )
