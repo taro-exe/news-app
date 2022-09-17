3d914a960d66be221480058648fb1fd8d2dd27c0
Rajiv Taro <rajivtarox@gmail.com>
2022-09-18 01:58:43 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -46,7 +46,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {
         this.setState({ page: this.state.page + 1 });
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=49e0395ed3eb4228b6380f6ea3ffb9b4&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults });
